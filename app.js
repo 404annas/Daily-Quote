b94f8235c15fde7c8d7c59f8f1bd2dce6414d6b2
Muhammad Annas Khan <annasking601@gmail.com>
2024-03-06 01:23:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const api_url = "https://api.quotable.io/random";
+const quoteBox = document.getElementById("quote");
+const authorName = document.getElementById("author");
+const newQuote = document.getElementById("new");
+
+async function fetchQuote(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  quoteBox.innerHTML = data.content;
+  authorName.innerHTML = data.author;
+}
+fetchQuote(api_url);
+
+newQuote.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetchQuote(api_url);
+});
+
+function linked() {
+    window.open("https://www.linkedin.com/feed/?shareActive=true&text=Hello World!", "LinkedIn Window", "width=600, height=300")
+}
