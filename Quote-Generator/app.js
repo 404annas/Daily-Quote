475f42dd1ad0d42e26958b8994bec2644ef00569
Muhammad Annas Khan <annasking601@gmail.com>
2024-03-06 02:08:48 +0500

    Update app.js

--- Quote-Generator/app.js
+++ Quote-Generator/app.js
@@ -1,7 +1,10 @@
 const api_url = "https://api.quotable.io/random";
+const random_api_url = "https://api.quotable.io/random";
 const quoteBox = document.getElementById("quote");
 const authorName = document.getElementById("author");
 const newQuote = document.getElementById("new");
+const inputBox = document.getElementById("input-box");
+const searchBtn = document.getElementById("search");
 
 async function fetchQuote(url) {
   const response = await fetch(url);
@@ -13,11 +16,37 @@ async function fetchQuote(url) {
 }
 fetchQuote(api_url);
 
+async function getRandomQuote(query) {
+  const response2 = await fetch(`${random_api_url}?tags=${query}`);
+  const data2 = await response2.json();
+  console.log(data2);
+
+  quoteBox.innerHTML = data2.content;
+  authorName.innerHTML = data2.author;
+}
+
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  if (inputBox.value === "") {
+    alert("Please Write A Keyword To Search A Quote For");
+  } else {
+    await getRandomQuote(inputBox.value);
+    inputBox.value = "";
+  }
+});
+
 newQuote.addEventListener("click", (e) => {
   e.preventDefault();
   fetchQuote(api_url);
 });
 
 function linked() {
-    window.open("https://www.linkedin.com/feed/?shareActive=true&text=" + quoteBox.innerHTML + "--- By --- " + authorName.innerHTML, "LinkedIn Window", "width=600, height=300")
+  window.open(
+    "https://www.linkedin.com/feed/?shareActive=true&text=" +
+      quoteBox.innerHTML +
+      "--- By --- " +
+      authorName.innerHTML,
+    "LinkedIn Window",
+    "width=600, height=300"
+  );
 }
